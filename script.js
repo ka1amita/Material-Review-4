811ff5b4dcc299073483dac55156cb9a7c85430d
ka1amita <kalamatej@gmail.com>
2023-03-20 17:38:12 +0100

    unknown

---
+++ script.js
@@ -0,0 +1,30 @@
+let age = 2023-1989
+console.log(age)
+let ww2 = 1945-1939
+console.log(ww2)
+
+// ARRAY
+let myArray = ['Claire', 'Mark', 'Susan'];
+let myOtherArray = [3, 12, true, 'Hello'];
+// Access value by index
+console.log(myArray[0]); // prints "Claire"
+
+// Change the value in a box
+myArray[0] = 'Peter'; // replaces "Claire" with "Peter"
+console.log(myArray[0])
+// Count of the boxes
+console.log(myArray.length); // prints 3
+
+// Add a new box with specific value
+myArray.push('Julia'); // adds Julia with a new index
+
+console.log(myArray.length); // prints 4
+
+// Exercis
+
+let names = [true, 1, [1], "a"]
+console.log(names[2])
+console.log(names.length > 4)
+
+console.log(names = names+"!")
+// OBJECT
